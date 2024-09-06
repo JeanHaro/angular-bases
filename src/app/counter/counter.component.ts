a8d3ff004f920fc5665659dfe808d2f6552d59fb
Jean Haro <JeanCHL98@hotmail.com>
2024-09-06 02:21:03 -0500

    43 - Funcionalidad del contador

--- src/app/counter/counter.component.ts
+++ src/app/counter/counter.component.ts
@@ -1,11 +1,33 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-counter',
-  template: '<h1>Hola Counter</h1>',
+  template: `
+    <h3>Counter: {{ counter }}</h3>
+
+    <button (click)="increaseBy(1)">+1</button>
+    <button (click)="resetCounter()">Reset</button>
+    <button (click)="decreaseBy(1)">-1</button>
+  `
 })
 
-
 export class CounterComponent {
+  constructor() { }
 
+  public counter: number = 10;
+
+  // Incrementar
+  increaseBy (value: number): void {
+    this.counter += value;
+  }
+
+  // Reseteo
+  resetCounter (): void {
+    this.counter = 10;
+  }
+
+  // Disminuir
+  decreaseBy (value: number): void {
+    this.counter -= value;
+  }
 }
