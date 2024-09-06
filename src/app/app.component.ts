5b00b7e3d8060602d4284275461801e0e7c32517
Jean Haro <JeanCHL98@hotmail.com>
2024-09-06 01:43:23 -0500

    41 - Contador

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string = 'Hola Mundo';
   public counter: number = 10;
+
+  // Incrementar
+  increaseBy (value: number): void {
+    this.counter += value;
+  }
+
+  // Reseteo
+  resetCounter (): void {
+    this.counter = 10;
+  }
+
+  // Disminuir
+  decreaseBy (value: number): void {
+    this.counter -= value;
+  }
 }
