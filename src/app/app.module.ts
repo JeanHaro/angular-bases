bcfd3129f879d2bfb0b5b2a9c3167eede4629ba5
Jean Haro <JeanCHL98@hotmail.com>
2024-09-06 02:36:18 -0500

    44 - Componente Hero y directorio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,15 @@ import { AppComponent } from './app.component';
 
 // Componentes
 import { CounterComponent } from './counter/counter.component';
+import { HeroComponent } from './heroes/hero/hero.component';
+import { ListComponent } from './heroes/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CounterComponent
+    CounterComponent,
+    HeroComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule,
